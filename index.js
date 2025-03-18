b6920b3f5731c385157ba0786937668afd14cc98
sairessy <sairessy@gmail.com>
2025-03-18 11:57:45 +0200

    Keeping alive on render

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import cron from "node-cron";
+import axios from "axios";
 import server from "./src/routes/index.js";
-import {env} from './src/config/index.js';
+import { env } from "./src/config/index.js";
 
 server.listen(env.PORT, async () => {
-  console.log('API working just fine.');
+  cron.schedule("*/3 * * * *", async () => {
+    try {
+      const response = await axios.get("https://api.advancedtechspace.com");
+      console.log(`Health check response: ${response.status}`);
+    } catch (error) {
+      console.error(`Health check error: ${error.message}`);
+    }
+  });
+  console.log("API working just fine.");
 });
